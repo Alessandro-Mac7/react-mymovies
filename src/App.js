6b66f975a06d76cc14c2308440cfa85a515e0932
alemac95 <alessandro.macri95@gmail.com>
2020-04-17 16:27:35 +0200

    add  function fetch API and list to view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = 'b2ca3bae';
+const API_URL = 'http://www.omdbapi.com/'
+
+function fetchMovies(search = 'troy'){
+  return fetch(API_URL + '?apikey=' + API_KEY + '&s=' + search).then( res => res.json());
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      totalCount: 0
+    }
+  }
+
+  componentDidMount(){
+    fetchMovies().then(res =>{
+      this.setState({
+        movies: res.Search
+      })
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>My favorite films</h1>
+        <ul>
+        {
+          this.state.movies.map(movie => <li key={movie.imbdID}>{movie.Title}</li> )
+        }
+        </ul>
+      </div>
+    );
+  }
+
 }
 
 export default App;
